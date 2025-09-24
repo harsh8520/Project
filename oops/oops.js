e8c820aa51ebde6c94579120136f55f6c01684f5
harsh8520 <harshasoriya258@gmail.com>
2025-09-24 16:17:02 +0530

    oops intro

--- oops/oops.js
+++ oops/oops.js
@@ -4,27 +4,27 @@
 
 
 //old tarika - function ka naam hamesha capital hona chahiye
-// function Toffee(flavor, color, taste) {
-//     this.flavor = flavor
-//     this.color = color
-//     this.taste = taste
-// }
+function Toffee(flavor, color, taste) {
+    this.flavor = flavor
+    this.color = color
+    this.taste = taste
+}
 
-// let obj1 = new Toffee('Chocolate', 'brown', 'sweet')
-// console.log(obj1);
+let obj = new Toffee('Chocolate', 'brown', 'sweet')
+console.log(obj1);
 
 //new tarika - classes ka use
-// class Toffee {
-//     constructor(flavor, color, taste) {
-//         this.flavor = flavor
-//         this.color = color
-//         this.taste = taste
-//     }
-// }
+class Toffee {
+    constructor(flavor, color, taste) {
+        this.flavor = flavor
+        this.color = color
+        this.taste = taste
+    }
+}
 
-// let obj1 = new Toffee('Chocolate', 'Brown', 'sweet')
-// let obj2 = new Toffee('Vanilla', 'White', 'sweet')
-// let obj3 = new Toffee('Coffee', 'darkBrown', 'sour')
-// console.log(obj1.flavor);
-// console.log(obj2.flavor);
-// console.log(obj3.flavor);
+let obj1 = new Toffee('Chocolate', 'Brown', 'sweet')
+let obj2 = new Toffee('Vanilla', 'White', 'sweet')
+let obj3 = new Toffee('Coffee', 'darkBrown', 'sour')
+console.log(obj1.flavor);
+console.log(obj2.flavor);
+console.log(obj3.flavor);
